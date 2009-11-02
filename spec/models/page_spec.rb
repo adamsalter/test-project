8bbe4893c90ad24a303432d49d9c0211c1c13cde
Adam Salter <adam@codebright.net>
2009-11-03 09:37:48 +1100

    - add fixtures for pages and users (tests failing)

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe Page do
+  fixtures :pages
+
   before(:each) do
     @valid_attributes = {
       :title => 'title'
     }
+  end
+
+  it "shouldn't have any users" do
+    User.first.should be_nil
   end
 
   it "should create a new instance given valid attributes" do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe User do
+  fixtures :users
+
   before(:each) do
     @valid_attributes = {
       :name => "name"
     }
+  end
+
+  it "shouldn't have any pages" do
+    Page.first.should be_nil
   end
 
   it "should create a new instance given valid attributes" do
